7c7ef4abac72612a7266f773ad98eb72bb90651c
Natali <n7483205@ya.ru>
2022-10-28 22:35:21 +0700

    EvenNumsQty

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] InitArray(int length)
+{
+    int[] resultArray = new int[length];
+    Random rnd = new Random{};
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = rnd.Next(100, 1000);
+    }
+    return resultArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int FindEvenNumsQty(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    if(array[i] % 2 == 0);
+    counter++;
+
+    return counter;
+}
+
+int[] arr = InitArray(length);
+PrintArray(arr);
+int count = FindEvenNumsQty(arr);
+Console.WriteLine($"Кол-во четных чисел в массиве = {count}. ");
